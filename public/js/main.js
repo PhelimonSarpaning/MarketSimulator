0d52804b4ad5838e98de90ce3276395fad4be791
SChakravorti21 <shoumyoarchit@gmail.com>
2018-01-23 19:10:49 -0500

    Added frontend validation to delete

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	var portName = $('#delete-game-button').attr('port-name');
+	console.log(portName);
+
 	$('#delete-game-button').on('click', function(e) {
 		e.preventDefault();
 
@@ -32,5 +35,30 @@ $(document).ready(function() {
 		}
 	});
 
-
+	$('.delete-game-form').validate({
+		rules: {
+			name: {
+				required: true,
+				equalTo: '#portName'
+			}
+		},
+		messages: {
+			name: {
+				required: "Please input the porfolio's name.",
+				equalTo: "The inputted name must match the portfolio name."
+			}
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "alert alert-danger mt-3" );
+			$('.input-parent').append(error);
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+		},
+		unhighlight: function (element, errorClass, validClass) {
+			$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+		}
+	})
 })
